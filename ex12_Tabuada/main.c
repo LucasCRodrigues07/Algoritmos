703c3c4fb08546b5d4d401d42efedce063b64b34
LucasCRodrigues07 <lucasemail43@gmail.com>
2025-09-13 23:31:49 -0300

    tabuada

--- ex12_Tabuada/main.c
+++ ex12_Tabuada/main.c
@@ -3,9 +3,11 @@
 int main(void)
 {
     int num;
-    printf("Digite um numero para ver sua tabuada: ");
+    printf("Digite um numero para ver sua tabuada: \n");
     scanf("%d", &num);
-    num = num * 1;
-    printf("A tabuada do numero digitado eh %d", num);
+    printf("A tabuada de %d eh:\n", num);
+    for (int i = 1; i <= 10; i++){
+        printf("%d * %d = %d\n", num, i, num * i);
+    }
     return 0;
 }
