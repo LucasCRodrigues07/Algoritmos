1a079727c938bd53a3d2c95d3b90bb67a711ee84
LucasCRodrigues07 <lucasemail43@gmail.com>
2025-09-15 20:06:54 -0300

    User and Password verification

--- ex22_PassVerificationAcessLvl/main.c
+++ ex22_PassVerificationAcessLvl/main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    char user[21];
-    char password[21];
-
-    return 0;
+    char senha[21];  //fflush e strcmp > fflush eu nsei bem mas, strcmp eh muito forte pois compara variaveis e se verdade, retorna 0
+    char usuario[21];
+    fflush(stdin);
+    printf("Digite usuário e senha: \n");
+    scanf("%s %s", &usuario, &senha);
+    if (strcmp(senha, "admin123")==0)
+    {
+     printf("Bem-vindo, %s. Nível de acesso: Administrador.", usuario);
+        return 0;
+    }
+    if (strcmp(senha, "user456")==0)
+    {
+        printf("Bem-vindo, %s. Nível de acesso: Padrão", usuario);
+    }
+    printf("Senha incorreta");
+        return 0;
 }
